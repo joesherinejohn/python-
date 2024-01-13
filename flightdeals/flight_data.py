39240bed32bfd5f30232d55b64496f3684293249
joesherinejohn <142822367+joesherinejohn@users.noreply.github.com>
2024-01-12 23:50:13 -0600

    Create flight_data.py

---
+++ flightdeals/flight_data.py
@@ -0,0 +1,14 @@
+import os
+class FlightData:
+    #This class is responsible for structuring the flight data.
+    def __init__(self, price, origin_city, origin_airport, via_city, stop_over,destination_city, destination_airport, out_date,
+                 return_date):
+        self.price = price
+        self.origin_city = origin_city
+        self.origin_airport = origin_airport
+        self.via_city = via_city
+        self.stop_over = stop_over
+        self.destination_city = destination_city
+        self.destination_airport = destination_airport
+        self.out_date = out_date
+        self.return_date = return_date
